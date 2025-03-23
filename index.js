e73b547e1a2bf5cd18b6a78db0edd1ece51c8a7e
George <george.kagwe@student.moringaschool.com>
2025-03-23 19:42:14 +0300

    event listening lab done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 function addingEventListener() {
+    const button = document.getElementById("button"); // Ensure this ID exists in the HTML
+    button.addEventListener("click", function() {
+        console.log("Button was clicked!");
+    });
 }
+
+
+
